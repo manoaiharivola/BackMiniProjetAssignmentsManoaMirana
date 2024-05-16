947cd5aad3af4f1e05123a0fbec0e22223ee0792
manoaiharivolaa <manoaiharivola@gmail.com>
2024-05-16 03:37:07 +0300

    Update passport.config.js

--- config/passport.config.js
+++ config/passport.config.js
@@ -4,6 +4,10 @@ let dotenv = require("dotenv");
 dotenv.config();
 
 let secret = process.env.JWT_SECRET;
+if (!secret) {
+  throw new Error("JWT_SECRET is not defined in the environment variables");
+}
+
 let jwtOptions = {
   secretOrKey: secret,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
